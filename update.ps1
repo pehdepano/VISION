0ffed4fa874233025de896342e928414e5aa4daa
pehdepano <44764365+pehdepano@users.noreply.github.com>
2023-01-13 17:53:21 -0300

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,4 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 $URL="https://raw.githubusercontent.com/pehdepano/PRTG/main/passagens.ps1"
-$PATH="C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\Powershell Scripts\passagens.ps1"
+$PATH="C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML\passagens.ps1"
 Invoke-Webrequest -URI $URL -OutFile $Path
