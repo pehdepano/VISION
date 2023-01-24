87f7f3453b703e44268c043201806567cec1b82f
pehdepano <44764365+pehdepano@users.noreply.github.com>
2023-01-24 08:41:37 -0300

    Update update.ps1
    Needs improvements.

--- update.ps1
+++ update.ps1
@@ -1,7 +1,5 @@
-[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
-$URL="https://raw.githubusercontent.com/pehdepano/PRTG/main/passagens.ps1"
-$PATH="C:\DGT\OCR\passagens.ps1"
-Invoke-Webrequest -URI $URL -OutFile $Path
-$URL="https://raw.githubusercontent.com/pehdepano/PRTG/main/config.ps1"
-$PATH="C:\DGT\OCR\config.ps1"
-Invoke-Webrequest -URI $URL -OutFile $Path
+#Update files --- needs atention
+#[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
+#$URL="https://001.com.link/update.zip"
+#$PATH="C:\001\001\update.zip"
+#Invoke-Webrequest -URI $URL -OutFile $Path
