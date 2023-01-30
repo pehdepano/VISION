31bef9c0fdad8fcb542373484028db29190665ee
pehdepano <44764365+pehdepano@users.noreply.github.com>
2023-01-30 13:22:21 -0300

    Delete update.ps1

--- update.ps1
+++
@@ -1,5 +0,0 @@
-#Update files --- needs atention
-#[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
-#$URL="https://001.com.link/update.zip"
-#$PATH="C:\001\001\update.zip"
-#Invoke-Webrequest -URI $URL -OutFile $Path
